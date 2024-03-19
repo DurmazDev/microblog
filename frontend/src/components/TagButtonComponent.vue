601a399d5526b73a45b05790b03bb652a5e26783
DurmazDev <aahmetdurmaz1@gmail.com>
2024-03-19 15:20:10 +0300

    feat: Tag feature frontend & some minimal fixes.

---
+++ frontend/src/components/TagButtonComponent.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="tag">
+    <div v-if="this.$route.name === 'home'">
+      <a
+        :href="`/?tag=${tag}`"
+        class="mx-0.5 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+      >
+        {{ tag }}
+      </a>
+    </div>
+    <div v-else>
+      <RouterLink
+        :to="`/?tag=${tag}`"
+        class="mx-0.5 bg-gray-200 hover:bg-gray-300 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center"
+      >
+        {{ tag }}
+      </RouterLink>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    mounted() {
+      console.log(this.$route);
+    },
+    props: {
+      tag: {
+        type: String,
+        default: null,
+        required: true,
+      },
+    },
+  };
+</script>
